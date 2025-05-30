ae1b726bfcbf00c8da31e0699b2be7ab5f4c00f9
NovaDevSas <novadevsas@gmail.com>
2025-05-30 09:55:54 -0500

    ios corrector

--- src/ios/AppPermissionsPlugin.h
+++ src/ios/AppPermissionsPlugin.h
@@ -1,6 +1,7 @@
 #import <Cordova/CDV.h>
 #import "AppDelegate.h"
 #import <CoreLocation/CoreLocation.h>
+#import <CoreBluetooth/CoreBluetooth.h>
 
 @interface AppPermissionsPlugin : CDVPlugin <CLLocationManagerDelegate>
 - (void)requestLocationPermission:(CDVInvokedUrlCommand*)command;
@@ -8,6 +9,8 @@
 - (void)checkLocationPermission:(CDVInvokedUrlCommand*)command;
 - (void)openURL:(CDVInvokedUrlCommand*)command;
 - (void)shouldShowRequestPermissionRationale:(CDVInvokedUrlCommand*)command;
+- (void)requestNearbyDevicesPermission:(CDVInvokedUrlCommand*)command;
+- (void)checkNearbyDevicesPermission:(CDVInvokedUrlCommand*)command;
 
 @property (nonatomic, strong) CLLocationManager* locationManager;
 @property (nonatomic, strong) CDVInvokedUrlCommand *command;
--- src/ios/AppPermissionsPlugin.m
+++ src/ios/AppPermissionsPlugin.m
@@ -3,14 +3,21 @@
 #import "AppDelegate.h"
 
 #import <CoreLocation/CoreLocation.h>
+#import <CoreBluetooth/CoreBluetooth.h>
 
 @implementation AppPermissionsPlugin
+{
+    CBCentralManager *centralManager;
+}
 
 - (void)pluginInitialize {
     NSLog(@"Starting App Permissions plugin");
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
     self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    // Initialize Bluetooth manager for permission checks
+    centralManager = [[CBCentralManager alloc] initWithDelegate:nil queue:nil options:@{CBCentralManagerOptionShowPowerAlertKey: @NO}];
 }
 
 - (void)requestLocationPermission:(CDVInvokedUrlCommand *)command {
@@ -52,19 +59,33 @@
 }
 
 - (void)checkLocationPermission:(CDVInvokedUrlCommand *)command {
-    if (@available(iOS 14.0, *)) {
-        NSUInteger authorizationStatus = [self.locationManager  authorizationStatus];
-        NSUInteger accuracyAuthorization = [self.locationManager accuracyAuthorization];
-        NSDictionary *locationData = @{
-            @"authorizationStatus": @(authorizationStatus),
-            @"desiredAccuracy": @(accuracyAuthorization)
-        };
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:locationData];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    } else {
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_NO_RESULT];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    BOOL accessFineLocation = NO;
+    BOOL accessCoarseLocation = NO;
+    BOOL accessBackgroundLocation = NO;
+    
+    CLAuthorizationStatus status = [CLLocationManager authorizationStatus];
+    
+    // In iOS, we don't have separate fine and coarse permissions like in Android
+    // Both are granted together with whenInUse or always authorization
+    if (status == kCLAuthorizationStatusAuthorizedWhenInUse || status == kCLAuthorizationStatusAuthorizedAlways) {
+        accessFineLocation = YES;
+        accessCoarseLocation = YES;
+        
+        // Check for background permission (always authorization)
+        if (status == kCLAuthorizationStatusAuthorizedAlways) {
+            accessBackgroundLocation = YES;
+        }
     }
+    
+    // Create a response similar to the Android implementation
+    NSDictionary *jsonObject = @{
+        @"ACCESS_FINE_LOCATION": @(accessFineLocation),
+        @"ACCESS_COARSE_LOCATION": @(accessCoarseLocation),
+        @"ACCESS_BACKGROUND_LOCATION": @(accessBackgroundLocation)
+    };
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:jsonObject];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
@@ -77,7 +98,72 @@
 }
 
 - (void)shouldShowRequestPermissionRationale:(CDVInvokedUrlCommand *)command {
-    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Not implemented"];
+    // iOS doesn't have an equivalent to Android's shouldShowRequestPermissionRationale
+    // But we can return a more useful response instead of "Not implemented"
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:NO];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)requestNearbyDevicesPermission:(CDVInvokedUrlCommand *)command {
+    // In iOS, Bluetooth permissions are implicitly requested when you start using CoreBluetooth
+    // We'll initialize the central manager if it's not already initialized
+    if (!centralManager) {
+        centralManager = [[CBCentralManager alloc] initWithDelegate:nil queue:nil options:@{CBCentralManagerOptionShowPowerAlertKey: @NO}];
+    }
+    
+    // Return success as the permission will be requested when needed
+    NSDictionary *response = @{
+        @"status": @"requested",
+        @"message": @"Permiso para dispositivos cercanos solicitado"
+    };
+    
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:response];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)checkNearbyDevicesPermission:(CDVInvokedUrlCommand *)command {
+    NSString *permissionStatus = @"unknown";
+    BOOL bluetoothPermission = NO;
+    
+    // Check Bluetooth authorization status
+    if (@available(iOS 13.0, *)) {
+        CBManagerAuthorization authStatus = [CBCentralManager authorization];
+        
+        switch (authStatus) {
+            case CBManagerAuthorizationAllowedAlways:
+                permissionStatus = @"granted";
+                bluetoothPermission = YES;
+                break;
+            case CBManagerAuthorizationDenied:
+                permissionStatus = @"denied";
+                break;
+            case CBManagerAuthorizationRestricted:
+                permissionStatus = @"restricted";
+                break;
+            case CBManagerAuthorizationNotDetermined:
+                permissionStatus = @"notDetermined";
+                break;
+            default:
+                permissionStatus = @"unknown";
+                break;
+        }
+    } else {
+        // Before iOS 13, there was no explicit Bluetooth permission
+        // We can only check if Bluetooth is powered on
+        if (centralManager.state == CBManagerStatePoweredOn) {
+            permissionStatus = @"granted";
+            bluetoothPermission = YES;
+        } else {
+            permissionStatus = @"unknown";
+        }
+    }
+    
+    NSDictionary *jsonObject = @{
+        @"BLUETOOTH_PERMISSION": @(bluetoothPermission),
+        @"BLUETOOTH_STATUS": permissionStatus
+    };
+    
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:jsonObject];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
